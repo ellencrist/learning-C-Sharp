e630a49798e2797f977b3e8d36363c6e365e40ba
Ellen Cristine <86074197+ellencrist@users.noreply.github.com>
2023-05-05 17:45:41 -0300

    Add files via upload

---
+++ Etec-ADS/looping.cs
@@ -0,0 +1,25 @@
+console.write(i); 'escreve lado a lado
+console.writeline(i); 'organizado pulando linha
+
+'contagem
+
+namespace estruturaform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            for (int i = 0; i <= 100000; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        
+        
+        
+        }
+    }
+}
